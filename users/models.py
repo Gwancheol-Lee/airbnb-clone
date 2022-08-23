115574f0f1b301264891b099017054470050c318
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-08-23 23:49:46 +0900

    User Model에 bio 항목 추가

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
 
-    pass
-    # bio = models.TextField()
+    bio = models.TextField(default="")
