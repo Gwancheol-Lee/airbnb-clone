bdbaa6c6f75446d58998e94a8ae50da250c4a189
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-08-24 23:59:30 +0900

    User 어드민 패널 커스텀

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from users import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "bio",
+                    "preference",
+                    "language",
+                    "fav_book_genre",
+                    "fav_movie_genre",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "preference",
+        "language",
+        "fav_book_genre",
+        "fav_movie_genre",
+    )
+
+    list_filter = (
+        "preference",
+        "language",
+        "fav_book_genre",
+        "fav_movie_genre",
+    )
