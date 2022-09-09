0a333cb02ce55ffd0f101ab384b984fcd15073a2
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-09-09 19:03:59 +0900

    admin 패널 list_display에 필드값 추가

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,6 @@ from users import models
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
 
-    pass
+    """admin 패널 display 필드값 추가"""
+
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
