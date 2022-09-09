501ba19874130331b3d220f053ee31feb1cb718b
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-09-09 18:14:27 +0900

    gender 필드에 choices 속성 추가, avatar,bio 필드에 blank=True(Unrequired) 속성 추가

--- users/models.py
+++ users/models.py
@@ -4,6 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
 
-    avatar = models.ImageField(null=True)
-    gender = models.CharField(max_length=10, null=True)
-    bio = models.TextField(default="")
+    GENDER_MALE = "Male"
+    GENDER_FEMALE = "Female"
+    GENDER_OTHER = "Other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
+    bio = models.TextField(default="", blank=True)
