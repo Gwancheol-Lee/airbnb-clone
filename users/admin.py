3361fb5df12efd9deb13932926a0bf6b9b24d8ee
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-09-09 19:57:41 +0900

    기존 장고 admin fieldsets + custom fieldsets 추가

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from users import models
 
 
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
 
-    """admin 패널 display 필드값 추가"""
+    """기존 장고 admin fieldsets + custom fieldsets 추가"""
 
-    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
