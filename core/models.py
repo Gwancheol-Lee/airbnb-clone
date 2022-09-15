604fe12c45ce338c60e765ebc702aeab2ca3f5ef
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-09-16 00:50:00 +0900

    core > models.py에 auto_now_add=True 및 auto_now=True 설정

--- core/models.py
+++ core/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 class TimeStampedModel(models.Model):
 
-    """Time Stamped Model"""
+    """
+    auto_now_add=True -> 새로운 Model을 만들면 장고가 현재 날짜랑 시간값을 created에 업데이트
+    auto_now=True -> Model을 매 번 저장할 때마다 장고가 현재 날짜랑 시간값을 updated에 업데이트
+    """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     """Meta Class는 기타 Property값 설정 등이 가능함. abstract=True는 해당 모델을 추상화 하여 결론적으로 DB에 등록되지 않으며 코드에만 사용된다."""
 
