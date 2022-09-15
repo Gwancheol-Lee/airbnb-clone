357c955201090b423e99f07b8e68d77cd30d829b
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-09-15 23:59:26 +0900

    core > models.py에 created와 updated DateTimeField 필드값 선언 후 Meta 클래스 사용하여 추상화 객체 선언

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+
+    """Time Stamped Model"""
+
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+    """Meta Class는 기타 Property값 설정 등이 가능함. abstract=True는 해당 모델을 추상화 하여 결론적으로 DB에 등록되지 않으며 코드에만 사용된다."""
+
+    class Meta:
+        abstract = True
