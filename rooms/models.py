69338210235b2ee6f729700c07126870942b3cf9
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-09-16 00:32:01 +0900

    rooms > models.py에 ForeignKey 사용하여 user <> room 연결

--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
+from users import models as user_models
 
 
 class Room(core_models.TimeStampedModel):
@@ -19,4 +20,5 @@ class Room(core_models.TimeStampedModel):
     baths = models.IntegerField()
     check_in = models.TimeField()
     check_out = models.TimeField()
-    instant_book = models.BooleanField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
