e95cc7f42df5bbe49680b6bea411ec57f4690515
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-10-02 20:23:05 +0900

    reviews Model 및 필요 필드값 생성

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,17 @@
+from logging.config import _LoggerConfiguration
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+class Review(core_models.TimeStampedModel):
+
+    """Reiview Model Definition"""
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanLiness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
