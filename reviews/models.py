21cd6cc18b7354cbac2b0cbec019d332e6806bfd
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-10-02 20:52:43 +0900

    Python3 Stirng 사용하여 str 리턴 함수에 두개 이상 필드값 리턴 설정

--- reviews/models.py
+++ reviews/models.py
@@ -16,4 +16,4 @@ class Review(core_models.TimeStampedModel):
     room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.review
+        return f'{self.review} - {self.room}'
