a500bb02ab235d0bb75a6b8e032a31291de08378
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-10-02 18:32:54 +0900

    Admin Pannel에 Room Type 추가

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from rooms import models
 
+@admin.register(models.RoomType)
+class ItemAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
